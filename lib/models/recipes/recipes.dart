3ac86acd840ff08c48cf1da9b4a954f58df16cde
NobtH <tranhungtb14@gmail.com>
2024-10-24 20:41:15 +0700

    Update

--- lib/models/recipes/recipes.dart
+++ lib/models/recipes/recipes.dart
@@ -3,12 +3,14 @@ class Recipes {
   String description;
   String imgPath;
   String rating;
+  String timeCooking;
   static const String defaultImagePath = 'assets/images/ingredient/default.png';
   Recipes(
       {required this.name,
       required this.description,
       required this.imgPath,
-      required this.rating});
+      required this.rating,
+      required this.timeCooking});
 
   String getImagePath() {
     return imgPath.isNotEmpty ? imgPath : defaultImagePath;
